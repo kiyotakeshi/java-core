4486dcc6a73b2f265f91f304dc2ed1e1d70d75c0
ponkan1219 <kiyotatakeshi.work@gmail.com>
2022-07-16 21:58:21 +0900

    add stream sample

--- src/test/java/org/example/StreamTests.java
+++ src/test/java/org/example/StreamTests.java
@@ -1,12 +1,10 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,13 +12,33 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
 
 class StreamTests {
 
     private int counter;
 
+    private List<Player> footballTeam;
+
+    private Map<String, String> books;
+
     private void wasCalled() {
         counter++;
+    }
+
+    @BeforeEach
+    void setUp() {
+        footballTeam = Arrays.asList(
+                new Player(33, "mike", 22),
+                new Player(11, "popcorn", 29),
+                new Player(28, "kendrick", 31),
+                new Player(30, "kanye", 22)
+        );
+
+        books = new HashMap<>();
+        books.put("978-0201633610", "Design patterns");
+        books.put("978-1617291999", "Java 8 in Action");
+        books.put("978-0134685991", "Effective Java");
     }
 
     @Test
@@ -149,9 +167,99 @@ class StreamTests {
         assertThat(reduced).isEqualTo(15);
     }
 
-    // TODO: https://www.baeldung.com/java-8-streams#2-the-collect-method
     @Test
-    void name() {
+    void reduce3() {
+        List<String> letters = Arrays.asList("a", "b", "c", "d", "e");
+        String result = letters.stream()
+                // .reduce("", (partialString, element) -> partialString + element);
+                .reduce("", String::concat);
 
+        assertThat(result).isEqualTo("abcde");
+    }
+
+    @Test
+    void reduce4() {
+        List<Human> users = Arrays.asList(new Human("John", 30), new Human("Julie", 35));
+
+        int computedAges =
+                users.stream()
+                        .reduce(0, (partialAgeResult, human)
+                                    -> partialAgeResult + human.getAge(), Integer::sum);
+
+        assertThat(computedAges).isEqualTo(65);
+    }
+
+    @Test
+    void collect() {
+        String collect = footballTeam.stream()
+                .map(Player::getName)
+                .collect(Collectors.joining(", ", "[", "]"));
+        assertThat(collect).isEqualTo("[mike, popcorn, kendrick, kanye]");
+    }
+
+    @Test
+    void summaryStatistics() {
+        IntSummaryStatistics summaryStatistics = footballTeam.stream()
+                .collect(Collectors.summarizingInt(Player::getAge));
+
+        System.out.println(summaryStatistics); // IntSummaryStatistics{count=4, sum=104, min=22, average=26.000000, max=31}
+        long expectedAverage = Math.round(summaryStatistics.getAverage());
+        assertThat(expectedAverage).isEqualTo(26L);
+    }
+
+    @Test
+    void groupingBy() {
+        Map<Integer, List<Player>> collect = footballTeam.stream()
+                .collect(Collectors.groupingBy(Player::getAge));
+
+        assertThat(collect.get(22).size()).isEqualTo(2);
+    }
+
+    @Test
+    void partitioningBy() {
+        Map<Boolean, List<Player>> collect = footballTeam.stream()
+                .collect(Collectors.partitioningBy(e -> e.getAge() >= 29));
+
+        assertThat(collect.get(true).size()).isEqualTo(2);
+    }
+
+    @Test
+    void mapStream() {
+        // key-value pair
+        // Set<Map.Entry<String, String>> entries = books.entrySet();
+        String optionalIsbn = books.entrySet().stream()
+                // .filter(e -> "Effective Java".equals(e.getValue()))
+                .filter(e -> e.getValue().startsWith("Effective Java"))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow();
+
+
+        assertThat(optionalIsbn).isEqualTo("978-0134685991");
+    }
+
+    @Test
+    void mapStream2() {
+        books.put("978-0321356680", "Effective Java: Second Edition");
+
+        List<String> isbnCodes = books.entrySet().stream()
+                .filter(e -> e.getValue().startsWith("Effective Java"))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        assertThat(isbnCodes.contains("978-0134685991")).isTrue();
+        assertThat(isbnCodes.contains("978-0321356680")).isTrue();
+    }
+
+    @Test
+    void mapStream3() {
+        List<String> titles = books.entrySet().stream()
+                .filter(e -> e.getKey().startsWith("978-0"))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+
+        assertThat(titles.size()).isEqualTo(2);
+        assertThat(titles.contains("Design patterns")).isTrue();
+        assertThat(titles.contains("Effective Java")).isTrue();
     }
 }
